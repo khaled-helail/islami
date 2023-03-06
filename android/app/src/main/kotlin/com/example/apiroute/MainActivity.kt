55b0690fec02f170b7620d75b6d2ab05280e790d
khaled-helail <khelail860@gmail.com>
2023-03-06 15:59:04 +0200

    initialize project added empty home Screen

---
+++ android/app/src/main/kotlin/com/example/apiroute/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.apiroute
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:apiroute/home_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      routes: {
+        HomeScreen.routeName:(_)=>HomeScreen()
+      },
+      initialRoute:HomeScreen.routeName ,
+    );
+  }
+
+
+  }
+
+
+
